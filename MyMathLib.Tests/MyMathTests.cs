f566be56ef07b1b940449dfc31106dafed8a850d
Fedir <timosek198@gmail.com>
2020-09-27 22:07:17 +0300

    first

---
+++ MyMathLib.Tests/MyMathTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyMathLib.Tests
+{
+    [TestClass]
+    public class MyMathTests
+    {
+        [TestMethod]
+        public void Sum_1and2_3returned()
+        {
+            int x = 1;
+            int y = 2;
+            int expected = 3;
+
+            Addition c = new Addition();
+            int actual = c.Sum(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ MyMathLib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyMathLib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyMathLib.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a379068f-c959-424b-b246-e81664b1fc29")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
